6919b40ef8284eb4679d4e41667768288eb77b77
MHelmiAzhar <helmiazhar311201@gmail.com>
2025-09-08 20:30:49 +0700

    fix: configuration for production

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
@@ -6,13 +6,29 @@ module.exports = {
     password: process.env.DB_PASS || null,
     database: process.env.DB_NAME || "data_cakra_dev",
     host: process.env.DB_HOST || "127.0.0.1",
-    dialect: "mysql"
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
   },
   test: {
     username: process.env.DB_USER || "root",
     password: process.env.DB_PASS || null,
     database: process.env.DB_NAME || "data_cakra_test",
     host: process.env.DB_HOST || "127.0.0.1",
-    dialect: "mysql"
-  }
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
